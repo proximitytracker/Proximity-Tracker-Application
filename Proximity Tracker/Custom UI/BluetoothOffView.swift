e76c283693aacb40c8a5a0efecc1cd18d44e983a
arnulfov <128873679+arnulfov@users.noreply.github.com>
2025-04-21 19:37:17 -0400

    Update BluetoothOffView.swift

--- Proximity Tracker/Custom UI/BluetoothOffView.swift
+++ Proximity Tracker/Custom UI/BluetoothOffView.swift
@@ -1,18 +1,13 @@
-//
-//  BluetoothOffView.swift
-//  Proximity Tracker
-//
+//  BluetoothOffView.swift (Proximity Tracker app)
 //  Created by user238598 on 4/2/24.
-//
 
 import SwiftUI
 
 struct ExclamationmarkView: View {
-    
+    // A view that shows a warning icon (exclamation mark inside a triangle)
     var body: some View {
         Image(systemName: "exclamationmark.triangle.fill")
-            .font(.system(size: 60, weight: .light, design: .default))
-            .padding(10)
-        
+            .font(.system(size: 60, weight: .light, design: .default)) // Use a large SF Symbol icon (size 60, light weight)
+            .padding(10) // Add padding around the icon for spacing
     }
 }
