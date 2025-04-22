e66914d23f6457d18d136098fb1bb190c4a38079
arnulfov <128873679+arnulfov@users.noreply.github.com>
2025-04-22 01:11:08 -0400

    Update DetailMoreView.swift

--- Proximity Tracker/SheetView/DetailMoreView.swift
+++ Proximity Tracker/SheetView/DetailMoreView.swift
@@ -8,43 +8,47 @@
 import SwiftUI
 
 struct DetailMoreView: View {
+    // Section with additional actions like NFC scan and support website
     
-    @ObservedObject var tracker: BaseDevice
-    @StateObject var nfcReader = NFCReader.sharedInstance
+    @ObservedObject var tracker: BaseDevice    // The tracker device for which to show additional options
+    @StateObject var nfcReader = NFCReader.sharedInstance    // NFC reader singleton for scanning NFC tags
     
     var body: some View {
         
         let constants = tracker.getType.constants
+        // Shortcut to tracker's type-specific constants
         
         let nfcSupport = constants.supportsNFC && !isiPad()
+        // Determine if NFC scanning is supported for this tracker (and not on iPad)
         
-        if(nfcSupport || constants.supportURL != nil) {
-            
+        if (nfcSupport || constants.supportURL != nil) {
             CustomSection(header: "more", footer: "more_trackerdetailview_description") {
-
                 let color = Color(#colorLiteral(red: 1, green: 0.6991065145, blue: 0.003071677405, alpha: 1))
+                // A specific shade of orange color for NFC scan button background
                 
                 Group {
                     if nfcSupport {
                         Button {
                             nfcReader.scan(infoMessage: String(format: "nfc_description".localized(), tracker.getName) )
+                            // Initiate NFC scan with an info message including the tracker's name
                         } label: {
                             NavigationLinkLabel(imageName: "person.fill", text: "scan_nfc", backgroundColor: color, isNavLink: true)
+                            // Label for NFC scan option with person icon
                         }
                         
                         if constants.supportURL != nil {
-                            CustomDivider()
+                            CustomDivider()    // Divider between NFC and website options
                         }
                     }
-                    
                     
                     if constants.supportURL != nil {
                         Button {
                             if let urlString = constants.supportURL, let url = URL(string: urlString) {
-                                openURL(url: url)
+                                openURL(url: url)    // Open the support URL in default browser
                             }
                         } label: {
                             NavigationLinkLabel(imageName: "info", text: "website_manufacturer", backgroundColor: .green, isNavLink: false)
+                            // Label for opening the manufacturer's support website
                         }
                     }
                 }
@@ -52,7 +56,6 @@ struct DetailMoreView: View {
         }
     }
 }
-
 
 struct Previews_TrackerInfoView_Previews: PreviewProvider {
     static var previews: some View {
